6f338936f48e475a319d0d34df5c8e5e8b9954c8
nameof <constexpr@foxmail.com>
2017-07-04 23:32:15 +0800

    客户端添加站点注销处理LogoutFilter、LogedSessionManager

---
+++ client/src/client/cas/component/LogedSessionManager.java
@@ -0,0 +1,26 @@
+package client.cas.component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.servlet.http.HttpSession;
+
+public class LogedSessionManager {
+	
+	private LogedSessionManager() {}
+	
+	private static final Map<String, HttpSession> logedSessions = new ConcurrentHashMap<>();
+
+	public static void attach(String token, HttpSession session) {
+		logedSessions.put(token, session);
+	}
+	
+	public static HttpSession detach(String token) {
+		return logedSessions.remove(token);
+	}
+	
+	public static HttpSession get(String token) {
+		if (token == null)
+			return null;
+		return logedSessions.get(token);
+	}
+}
---
+++ client/src/client/filter/LogoutFilter.java
@@ -0,0 +1,39 @@
+package client.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import client.cas.component.LogedSessionManager;
+
+public class LogoutFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp,
+			FilterChain arg2) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		String token = request.getParameter("token");
+		HttpSession session = LogedSessionManager.get(token);
+		if (session != null) {
+			session.invalidate();
+		}
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+
+}
